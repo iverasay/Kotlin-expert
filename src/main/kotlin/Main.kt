3ffd44264c11d3f5363fcb75bbede20b47736031
Ignacio Verasay <ignacio.verasay@ypsomed.com>
2024-11-12 15:18:58 +0100

    Exercise 2 - resuelto

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,11 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.DragData
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -17,18 +14,19 @@ import androidx.compose.ui.window.application
 @Preview
 fun App() {
     val text = remember { mutableStateOf("") }
-    val message = "Hello ${text.value}"
     val buttonEnable: Boolean = text.value.isNotEmpty()
 
     MaterialTheme {
         Column {
             TextField(value = text.value , onValueChange = { newText -> text.value = newText })
-            Text(text = message)
+            Text(text = buildMessage(text.value))
             Button(onClick = { text.value = ""}, enabled = buttonEnable) { Text("Clean") }
         }
 
     }
 }
+
+fun buildMessage(msg: String): String = "Hello $msg"
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "My Application") {
